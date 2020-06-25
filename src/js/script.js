4d1ead1f5a1468cc20cde75d26d4a57a6254ee57
des-yogi <nuevo-design@mail.ru>
2020-06-25 21:37:41 +0300

    index in progress

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// const ready = require('./utils/documentReady.js');
+const ready = require('./utils/documentReady.js');
 
 // ready(function(){
 //   console.log('DOM героически построен!');
@@ -15,9 +15,32 @@ $( document ).ready(function() {
 
     if ( scrolled > 100 && scrolled > scrollPrev ) {
       header.addClass('out');
-    } else {
+    }
+    else if ( scrolled > scrollPrev && scrolled != 0 ) {
+      header.addClass('in-active');
+    }
+    else if ( scrolled < 100 ) {
+      header.removeClass('in-active');
+    }
+    else {
       header.removeClass('out');
     }
     scrollPrev = scrolled;
+  });
+});
+
+const Blazy = require('blazy');
+
+ready(function(){
+  var blazy = new Blazy({
+      selector: '.b-lazy'
+  });
+});
+
+$( document ).ready(function() {
+  const tilt = $('.js-tilt').tilt({
+    perspective:    500, // чем больше цирфа, тем меньше искажение
+    scale:          1.2,
+    speed:          1500
   });
 });
